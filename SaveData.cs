cd16a753f24e79e917ae9dc1ddfa54183b849cd4
Ákos Nikházy <nikhazyakos@tutanota.com>
2023-04-04 08:06:23 +0200

    Create SaveData.cs

---
+++ SaveData.cs
@@ -0,0 +1,14 @@
+/// <summary>
+/// Class <c>SaveData</c> keeps the game data on runtime
+/// </summary>
+public class SaveData
+{
+    public int level = 1;
+
+    public string lang = "eng";
+
+    public int lv1scr  = 0;
+    public int lv2scr  = 0;
+    public int lv3scr  = 0;
+    public int lv4scr  = 0;
+}
